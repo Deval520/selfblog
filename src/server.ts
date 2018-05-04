9329aabd52e0c956dc7c65acac9e70f0cf8fb934
fanchen <fanchen@pinduoduo.com>
2018-05-04 20:16:13 +0800

    delete .

--- src/server.ts
+++
@@ -1,5 +0,0 @@
-import { app } from './app'
-import http from 'http'
-
-export default http.createServer(app.callback()).listen(process.env.PORT || 3000)
-console.log(`>server start at: http://localhost:3000`)
